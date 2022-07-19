166db6089e0a50cae3445149d108bd129857bf79
firdaus <a.firdaus@gmail.com>
2022-07-19 14:39:20 +0700

    push all dao minus thread

---
+++ community/src/main/java/com/lawencon/community/dao/ActivityCategoryDao.java
@@ -0,0 +1,8 @@
+package com.lawencon.community.dao;
+
+import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.ActivityCategory;
+
+public class ActivityCategoryDao extends AbstractJpaDao<ActivityCategory>{
+
+}
---
+++ community/src/main/java/com/lawencon/community/dao/IndustryDao.java
@@ -0,0 +1,8 @@
+package com.lawencon.community.dao;
+
+import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.Industry;
+
+public class IndustryDao extends AbstractJpaDao<Industry>{
+
+}
