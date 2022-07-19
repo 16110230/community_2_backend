a32b06190c48fd9313c3715239cae370259847a0
copi <mtaufikdhrmwn@gmail.com>
2022-07-19 16:50:53 +0700

    add thread details and users dao

--- community/src/main/java/com/lawencon/community/dao/ThreadDetailsDao.java
+++ community/src/main/java/com/lawencon/community/dao/ThreadDetailsDao.java
@@ -1,5 +1,53 @@
 package com.lawencon.community.dao;
 
-public class ThreadDetailsDao {
+import java.util.ArrayList;
+import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
+import com.lawencon.base.AbstractJpaDao;
+import com.lawencon.community.model.Thread;
+import com.lawencon.community.model.ThreadDetails;
+import com.lawencon.community.model.Users;
+
+@Repository
+public class ThreadDetailsDao extends AbstractJpaDao<ThreadDetails> {
+	public List<ThreadDetails> findByThreadId(String id) {
+		
+		StringBuilder sqlBuilder = new StringBuilder()
+				.append("SELECT t.id, u.id, u.fullName, td.thread_desc ")
+				.append("FROM thread_details td ")
+				.append("INNER JOIN thread t ON t.id = td.thread_id ")
+				.append("INNER JOIN users u ON u.id = u.user_id ")
+				.append("WHERE t.id = :id");
+		
+		List<ThreadDetails> response = new ArrayList<ThreadDetails>();
+		try {			
+			List<?> result = createNativeQuery(sqlBuilder.toString())
+					.setParameter("id", id)
+					.getResultList();		
+			if(result != null) {
+				result.forEach(obj -> {					
+					Object[] objArr = (Object[]) obj;
+					ThreadDetails detail = new ThreadDetails();
+					Thread header = new Thread();
+					Users user = new Users();
+					
+					header.setId(objArr[0].toString());
+					user.setId(objArr[1].toString());
+					user.setFullName(objArr[2].toString());
+					detail.setThreadDesc(objArr[3].toString());
+					
+					detail.setThread(header);
+					detail.setUser(user);
+					
+					response.add(detail);
+				});
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return response;
+	}
 }
--- community/src/main/java/com/lawencon/community/dao/UsersDao.java
+++ community/src/main/java/com/lawencon/community/dao/UsersDao.java
@@ -1,5 +1,9 @@
 package com.lawencon.community.dao;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Repository;
 
 import com.lawencon.base.AbstractJpaDao;
@@ -9,7 +13,7 @@ import com.lawencon.community.model.Users;
 @Repository
 public class UsersDao extends AbstractJpaDao<Users> {
 	
-	public Users findByUsernameAndPassword(String username, String password) {
+	public Users findByUsernameAndPassword(String username, String userPassword) {
 		Users response = new Users();
 		
 		StringBuilder sqlBuilder = new StringBuilder()
@@ -19,7 +23,11 @@ public class UsersDao extends AbstractJpaDao<Users> {
 				.append("WHERE username = :username AND u.user_password = :password");
 		
 		try {			
-			Object result = createNativeQuery(sqlBuilder.toString()).getSingleResult();
+			Object result = createNativeQuery(sqlBuilder.toString())
+					.setParameter("username", username)
+					.setParameter("password", userPassword)
+					.getSingleResult();
+			
 			if(result != null) {
 				Object[] objArr = (Object[]) result;
 				
@@ -35,5 +43,32 @@ public class UsersDao extends AbstractJpaDao<Users> {
 		}
 		
 		return response;
+	}
+	
+	public List<Users> findAllByToday() {
+		StringBuilder sqlBuilder = new StringBuilder()
+				.append("SELECT fullName ")
+				.append("FROM users ")
+				.append("WHERE created_at = :date");
+		
+		List<Users> users = new ArrayList<Users>();
+		
+		try {			
+			List<?> result = createNativeQuery(sqlBuilder.toString())
+					.setParameter("date", LocalDateTime.now())
+					.getResultList();
+			
+			result.forEach(obj -> {
+				Object[] objArr = (Object[]) obj;
+				Users user = new Users();
+				
+				user.setFullName(objArr[0].toString());
+				users.add(user);
+			});
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return users;
 	}
 }
