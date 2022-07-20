a0d4b46188cb6f5fedb12bcf9108a6db40239c1f
firdaus <a.firdaus@gmail.com>
2022-07-20 11:02:44 +0700

    add specific dao for count

--- community/src/main/java/com/lawencon/community/dao/ThreadDao.java
+++ community/src/main/java/com/lawencon/community/dao/ThreadDao.java
@@ -7,5 +7,22 @@ import com.lawencon.community.model.Thread;
 
 @Repository
 public class ThreadDao extends AbstractJpaDao<Thread>{
-
+	
+	public long countAllNewThreadToday() {
+		StringBuilder sqlBuilder = new StringBuilder()
+				.append("SELECT COUNT(id) new_thread_today ")
+				.append("FROM thread")
+				.append("WHERE DATE(created_at) = DATE(NOW())");
+		
+		Long response = 0l;
+		
+		Object result = createNativeQuery(sqlBuilder.toString())
+				.getSingleResult();
+		
+		if(result != null) {
+			response = Long.valueOf(result.toString());
+		}
+		
+		return response;
+	}
 }
