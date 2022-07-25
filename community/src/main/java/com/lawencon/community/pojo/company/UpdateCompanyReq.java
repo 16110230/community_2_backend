57e4e675ccfc0a91a083797f7c32f4e45ea0fdf4
firdaus <a.firdaus@gmail.com>
2022-07-25 15:27:58 +0700

    oke

--- community/src/main/java/com/lawencon/community/pojo/company/UpdateCompanyReq.java
+++ community/src/main/java/com/lawencon/community/pojo/company/UpdateCompanyReq.java
@@ -4,58 +4,80 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
 public class UpdateCompanyReq {
-	
+
 	@NotNull(message = "Id can't be null")
 	private String id;
-	
+
 	@NotBlank(message = "Company name can't be empty")
 	private String companyName;
-	
+
 	@NotBlank(message = "Company code can't be empty")
 	private String companyCode;
-	
+
 	@NotBlank(message = "Company address can't be empty")
 	private String address;
-	
+
+	@NotBlank(message = "Message can't be empty")
+	private String Email;
+
 	@NotNull(message = "Version company can't be null")
 	private Integer version;
-	
+
 	@NotNull(message = "Is active company can't be null")
 	private Boolean isActive;
-	
+
+	public String getEmail() {
+		return Email;
+	}
+
+	public void setEmail(String email) {
+		Email = email;
+	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setId(String id) {
 		this.id = id;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public String getCompanyName() {
 		return companyName;
 	}
+
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
+
 	public String getCompanyCode() {
 		return companyCode;
 	}
+
 	public void setCompanyCode(String companyCode) {
 		this.companyCode = companyCode;
 	}
+
 	public Integer getVersion() {
 		return version;
 	}
+
 	public void setVersion(Integer version) {
 		this.version = version;
 	}
+
 	public Boolean getIsActive() {
 		return isActive;
 	}
+
 	public void setIsActive(Boolean isActive) {
 		this.isActive = isActive;
 	}
