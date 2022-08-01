71d58d1e940cc3ba3bbca72e8681ae2f6008fc3a
Shifda Ichfani <shifda.ichfani@gmail.com>
2022-08-01 18:04:18 +0700

    1-8-22 backend afternoon

---
+++ community/src/main/java/com/lawencon/community/pojo/dashboard/ShowDashboard.java
@@ -0,0 +1,13 @@
+package com.lawencon.community.pojo.dashboard;
+
+public class ShowDashboard {
+	private PojoDashboard data;
+	
+	public void setData(PojoDashboard data) {
+		this.data = data;
+	}
+	
+	public PojoDashboard getData() {
+		return data;
+	}
+}
